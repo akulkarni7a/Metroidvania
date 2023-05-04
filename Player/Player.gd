eae83e94ef2e715f62e86ea036da141e9da1c15d
Anirudh Kulkarni <askakdagr8@gmail.com>
2023-05-03 21:58:21 -0500

    working on player input

--- Player/Player.gd
+++ Player/Player.gd
@@ -10,18 +10,27 @@ export (int) var MAX_SLOPE_ANGLE = 46
 var motion = Vector2.ZERO
 
 func _physics_process(delta):
-	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	var input_vector = get_input_vector()
+	apply_horizontal_force(input_vector,delta)
+	move()
 	
-	if input_vector != Vector2.ZERO:
-		motion += input_vector * ACCELERATION * delta
-		motion = motion.clamped(MAX_SPEED)
-	else:
-		motion = motion.linear_interpolate(Vector2.ZERO,FRICTION)
-		
-		
-	motion = move_and_slide(motion)
 
 func get_input_vector():
-	
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	return input_vector
+
+func apply_horizontal_force(input_vector, delta):
+	if input_vector.x != 0:
+		motion.x += input_vector.x * ACCELERATION * delta
+		motion.x = clamp(motion.x,-MAX_SPEED,MAX_SPEED)
+
+func apply_friction(input_vector):
+	if input_vector.x == 0:
+		motion.x = lerp(motion.x,0,FRICTION)
+		
+func jump_check():
+	pass
+
+func move():
+	motion = move_and_slide(motion)
