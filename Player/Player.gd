6d9ffc0f6f1d9ae586ac63345a2120e2dcc4efe1
Anirudh Kulkarni <askakdagr8@gmail.com>
2023-05-09 21:07:28 -0500

    basic character movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,10 +8,21 @@ export (int) var JUMP_FORCE = 128
 export (int) var MAX_SLOPE_ANGLE = 46
 
 var motion = Vector2.ZERO
+var snap_vector = Vector2.ZERO
+var just_jumped = false
+
+onready var sprite = $Sprite
+onready var spriteAnimator = $SpriteAnimator
 
 func _physics_process(delta):
+	just_jumped = false
 	var input_vector = get_input_vector()
 	apply_horizontal_force(input_vector,delta)
+	apply_friction(input_vector)
+	update_snap_vector()
+	jump_check()
+	apply_gravity(delta)
+	update_animations(input_vector)
 	move()
 	
 
@@ -26,11 +37,48 @@ func apply_horizontal_force(input_vector, delta):
 		motion.x = clamp(motion.x,-MAX_SPEED,MAX_SPEED)
 
 func apply_friction(input_vector):
-	if input_vector.x == 0:
+	if input_vector.x == 0 and is_on_floor():
 		motion.x = lerp(motion.x,0,FRICTION)
 		
-func jump_check():
-	pass
+func update_snap_vector():
+	if is_on_floor():
+		snap_vector = Vector2.DOWN
 
+func jump_check():
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			just_jumped = true
+			motion.y =- JUMP_FORCE
+			motion.y = min(motion.y,JUMP_FORCE)
+			snap_vector = Vector2.ZERO
+	else:
+		if Input.is_action_just_released("ui_up") and motion.y <- JUMP_FORCE/2:
+			motion.y = -JUMP_FORCE/2
+			
+func apply_gravity(delta):
+	if not is_on_floor():
+		motion.y += GRAVITY * delta
+
+func update_animations(input_vector):
+	if input_vector.x != 0:
+		sprite.scale.x = sign(input_vector.x)
+		spriteAnimator.play("Run")
+	else:
+		spriteAnimator.play("Idle")
+
+	if not is_on_floor():
+		spriteAnimator.play("Jump")
+	
 func move():
-	motion = move_and_slide(motion)
+	var was_in_air = not is_on_floor()
+	var was_on_floor = is_on_floor()
+	var last_motion = motion 
+	
+	motion = move_and_slide_with_snap(motion,snap_vector*4,Vector2.UP,true,4,deg2rad(MAX_SLOPE_ANGLE))
+	#landing
+	if was_in_air and is_on_floor():
+		motion.x = last_motion.x
+	
+	#just left ground
+	if was_on_floor and not is_on_floor() and not just_jumped:
+		motion.y = 0 
